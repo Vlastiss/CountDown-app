016da288e6ace25fbab216d9d976cd96e8893690
Vlastiss <120052173+Vlastiss@users.noreply.github.com>
2024-01-18 19:14:51 +0000

    setup

---
+++ script.js
@@ -0,0 +1,9 @@
+const currentDate = new Date();
+const flyDate = "26 jan 2024"
+
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+
