7293510c73280ef52b5a20c6c43bd1151a3e072c
Vlastiss <120052173+Vlastiss@users.noreply.github.com>
2024-01-18 19:49:25 +0000

    final version

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-const currentDate = new Date();
-const flyDate = "26 jan 2024"
+const eldays = document.getElementById('days')
+const elhours = document.getElementById('hours')
+const elminutes = document.getElementById('minutes')
+const elseconds = document.getElementById('seconds')
 
-const days = document.getElementById('days');
-const hours = document.getElementById('hours');
-const minutes = document.getElementById('minutes');
-const seconds = document.getElementById('seconds');
+const flight = " 26 jan 2024 08:15:00"
+
+function countdown() {
+    const flyDate = new Date(flight)
+    const currentDate = new Date();
+
+    const totalSeconds = (flyDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
 
 
+    eldays.innerHTML = days;
+    elhours.innerHTML = hours;
+    elminutes.innerHTML = minutes;
+    elseconds.innerHTML = seconds;
+}
+
+
+
+countdown();
+
+setInterval(countdown, 1000);
